fe74f701d7376f779b8ac0a8a464d244738a5ba8
der20077 <danilvedenapin2@gmail.com>
2026-07-03 15:55:04 +0300

    Add files via upload

---
+++ HomePage.jsx
@@ -0,0 +1,27 @@
+import "./HomePage.scss";
+
+function HomePage({ users }) {
+  return (
+    <ul className="users">
+      {users.map((user) => {
+        const hobbis = user.hobbies;
+        return (
+          <li className="card" key={user.id}>
+            <h1 className="card__name">{user.name}</h1>
+            <p>Возраст {user.age}</p>
+            <p>Хобби</p>
+            <ul>
+              {hobbis.length > 0 ? (
+                hobbis.map((hobbi, index) => <li key={index}>{hobbi}</li>)
+              ) : (
+                <p className="card__not-hobby">Нет хобби</p>
+              )}
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default HomePage;
