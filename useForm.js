ae11bf72625a1f0a58c83d468bf436a589d0cf9c
der20077 <danilvedenapin2@gmail.com>
2026-07-24 11:39:59 +0300

    Add files via upload

---
+++ useForm.js
@@ -0,0 +1,29 @@
+import { useDebugValue, useState } from "react";
+
+const useForm = (initialState) => {
+  const [formData, setFormData] = useState(initialState);
+
+  useDebugValue(formData, (data) => {
+    return `Данные из формы ${JSON.stringify(data)}`;
+  });
+
+  const handleChange = (key, value) => {
+    setFormData({
+      ...formData,
+      [key]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return formData;
+  };
+
+  return {
+    formData,
+    handleChange,
+    handleSubmit,
+  };
+};
+
+export default useForm;
---
+++ useLocalStorage.js
@@ -0,0 +1,45 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const getStoredValue = () => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error("Ошибка чтения данных из localStorage", error);
+      return initialValue;
+    }
+  };
+
+  const [storedValue, setStoredValue] = useState(getStoredValue);
+
+  const setValue = (value) => {
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Ошибка добавления в localStorage", error);
+    }
+  };
+
+  const removeValue = () => {
+    try {
+      localStorage.removeItem(key);
+      setStoredValue(initialValue);
+    } catch (error) {
+      console.error("Ошибка удаления из localStorage", error);
+    }
+  };
+
+  useEffect(() => {
+    setStoredValue(getStoredValue());
+  }, [key]);
+
+  useDebugValue(
+    { key, storedValue },
+    ({ key, storedValue }) =>
+      `Key: ${key}, Value: ${JSON.stringify(storedValue)}`,
+  );
+
+  return [storedValue, setValue, removeValue];
+};
